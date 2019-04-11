0e8eb90b173f523fe1ac106e90d997d2e5cf6ddd
John Sullivan <sulljohn12@gmail.com>
2019-04-11 00:11:59 -0400

    Updated .gitignore

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+
+let num=0;
+
+// Source: https://stackoverflow.com/questions/5638783/how-do-i-get-this-javascript-to-run-every-second
+$(function(){
+setInterval(oneSecondFunction, 1000);
+});
+
+function oneSecondFunction() {
+	let text = `You've been on this page for ${num} seconds.`
+
+	$("body").empty();
+	$("body").append("<div>" + text + "</div>");
+	num++;
+}
