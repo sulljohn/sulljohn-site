e2199646bab27510f569ef2a072795c8816b850d
John Sullivan <sulljohn12@gmail.com>
2019-04-26 02:08:34 -0400

    Incorporated from redux short assignment

---
+++ src/components/app.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import '../style.scss';
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
+import Counter from '../containers/counter';
+import Controls from '../containers/controls';
+
+
+const About = () => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = () => {
+  return (
+    <div>
+      <div>Welcome</div>
+      <Counter />
+      <Controls />
+    </div>
+  );
+};
+
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+const FallBack = () => {
+  return <div>URL Not Found</div>;
+};
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
