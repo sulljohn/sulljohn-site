9b4198009aa37f105481b312ca169d9d664c55e4
John Sullivan <sulljohn12@gmail.com>
2019-06-10 20:42:28 -0400

    Fixed some minor issues

--- src/components/artwork.js
+++ src/components/artwork.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const Artwork = () => {
   return <div className="placeholder">Coming Soon!</div>;
 };
 
-export default Artwork
+export default Artwork;
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const Projects = () => {
   return <div className="placeholder">Coming Soon!</div>;
 };
 
-export default Projects
+export default Projects;
