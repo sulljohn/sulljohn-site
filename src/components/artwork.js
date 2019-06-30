894c76e2bb10ab3a46affd004931eade70aa7e10
John Sullivan <sulljohn12@gmail.com>
2019-06-30 11:54:46 -0400

    Fixed mobile formatting; added more with art portfolio

--- src/components/artwork.js
+++ src/components/artwork.js
@@ -10,6 +10,12 @@ const IMAGES = [{
   caption: 'After Rain (Jeshu John - designerspics.com)',
 },
 {
+  src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
+  thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
+  thumbnailWidth: 320,
+  thumbnailHeight: 212,
+},
+{
   src: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
   thumbnail: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg',
   thumbnailWidth: 320,
@@ -17,18 +23,29 @@ const IMAGES = [{
   tags: [{ value: 'Ocean', title: 'Ocean' }, { value: 'People', title: 'People' }],
   caption: 'Boats (Jeshu John - designerspics.com)',
 },
-
 {
   src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
   thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
   thumbnailWidth: 320,
   thumbnailHeight: 212,
-}];
+},
+];
 
 const Artwork = () => {
   return (
-    <div className="placeholder">
-      <Gallery images={IMAGES} />,
+    <div className="artContainer">
+      <div className="artSection">
+        <h1>Posters</h1>
+        <Gallery images={IMAGES} />
+      </div>
+      <div className="artSection">
+        <h1>Drawings</h1>
+        <Gallery images={IMAGES} />
+      </div>
+      <div className="artSection">
+        <h1>Paintings</h1>
+        <Gallery images={IMAGES} />
+      </div>
     </div>
   );
 };
