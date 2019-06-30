9f78730ea1b7f6a8638a67b3686eff7de745eaf8
John Sullivan <sulljohn12@gmail.com>
2019-06-30 19:41:58 -0400

    Setting spacing based on size works well; other parts coming along on projects and artwork

--- src/components/artwork.js
+++ src/components/artwork.js
@@ -6,7 +6,6 @@ const IMAGES = [{
   thumbnail: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg',
   thumbnailWidth: 320,
   thumbnailHeight: 174,
-  isSelected: true,
   caption: 'After Rain (Jeshu John - designerspics.com)',
 },
 {
@@ -14,13 +13,13 @@ const IMAGES = [{
   thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
   thumbnailWidth: 320,
   thumbnailHeight: 212,
+  caption: 'Text',
 },
 {
   src: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
   thumbnail: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg',
   thumbnailWidth: 320,
   thumbnailHeight: 212,
-  tags: [{ value: 'Ocean', title: 'Ocean' }, { value: 'People', title: 'People' }],
   caption: 'Boats (Jeshu John - designerspics.com)',
 },
 {
@@ -36,15 +35,15 @@ const Artwork = () => {
     <div className="artContainer">
       <div className="artSection">
         <h1>Posters</h1>
-        <Gallery images={IMAGES} />
+        <Gallery images={IMAGES} enableImageSelection={false} />
       </div>
       <div className="artSection">
         <h1>Drawings</h1>
-        <Gallery images={IMAGES} />
+        <Gallery images={IMAGES} enableImageSelection={false} />
       </div>
       <div className="artSection">
         <h1>Paintings</h1>
-        <Gallery images={IMAGES} />
+        <Gallery images={IMAGES} enableImageSelection={false} />
       </div>
     </div>
   );
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+const projects = [
+  {
+    title: 'title1',
+    image: 'src/res/imgs/artwork.jpg',
+    text: 'text1',
+  },
+  {
+    title: 'title2',
+    image: 'src/res/imgs/horizon.jpg',
+    text: 'text2',
+  },
+];
 
 const Projects = () => {
-  return <div className="placeholder">Coming Soon!</div>;
+  return (
+    <div className="projects">
+      <h1>Projects</h1>
+      <div className="projectList">
+        {projects.map(item => (
+          <div className="project">
+            <img src={item.image} alt={item.image} className="projectImage" />
+            <div className="projectText">
+              <div>{item.title}</div>
+              <div>{item.text}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
