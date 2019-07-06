b8e7acf33158c4012c54d272c9a3150417856dfa
John Sullivan <sulljohn12@gmail.com>
2019-07-06 00:53:17 -0400

    Final styling fixes for tonight ...

--- src/components/artwork.js
+++ src/components/artwork.js
@@ -59,6 +59,12 @@ const IMAGES = [{
   thumbnailWidth: 320,
   thumbnailHeight: 212,
 },
+{
+  src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
+  thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
+  thumbnailWidth: 320,
+  thumbnailHeight: 212,
+}
 ];
 
 const Artwork = () => {
