2fd7b1f2e2e9d9435c238e9858f1e86131688a3c
John Sullivan <sulljohn12@gmail.com>
2019-07-06 10:42:21 -0400

    Separated out data into other files

--- src/components/artwork.js
+++ src/components/artwork.js
@@ -1,89 +1,19 @@
 import React from 'react';
 import Gallery from 'react-grid-gallery';
 import Footer from './footer';
-
-const posters = [{
-  src: 'src/res/artwork/1_posters/1_Superhero.jpg',
-  thumbnail: 'src/res/artwork/1_posters/1_Superhero.jpg',
-  thumbnailWidth: 144,
-  thumbnailHeight: 212,
-  caption: 'Superheros vs. Villains',
-},
-{
-  src: 'src/res/artwork/1_posters/2_Seuss.jpg',
-  thumbnail: 'src/res/artwork/1_posters/2_Seuss.jpg',
-  thumbnailWidth: 170,
-  thumbnailHeight: 212,
-  caption: 'Seuss on the Loose',
-},
-{
-  src: 'src/res/artwork/1_posters/3_Icecraft.jpg',
-  thumbnail: 'src/res/artwork/1_posters/3_Icecraft.jpg',
-  thumbnailWidth: 141,
-  thumbnailHeight: 212,
-  caption: 'Icecraft and Blizzardry',
-},
-{
-  src: 'src/res/artwork/1_posters/4_Snow_Wars.jpg',
-  thumbnail: 'src/res/artwork/1_posters/4_Snow_Wars.jpg',
-  thumbnailWidth: 141,
-  thumbnailHeight: 212,
-  caption: 'Snow Wars',
-},
-];
-
-const IMAGES = [{
-  src: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg',
-  thumbnail: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg',
-  thumbnailWidth: 320,
-  thumbnailHeight: 174,
-  caption: 'After Rain (Jeshu John - designerspics.com)',
-},
-{
-  src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
-  thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
-  thumbnailWidth: 320,
-  thumbnailHeight: 212,
-  caption: 'Text',
-},
-{
-  src: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
-  thumbnail: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg',
-  thumbnailWidth: 320,
-  thumbnailHeight: 212,
-  caption: 'Boats (Jeshu John - designerspics.com)',
-},
-{
-  src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
-  thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
-  thumbnailWidth: 320,
-  thumbnailHeight: 212,
-},
-{
-  src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
-  thumbnail: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
-  thumbnailWidth: 320,
-  thumbnailHeight: 212,
-}
-];
+import artwork from '../data/artwork';
 
 const Artwork = () => {
   return (
     <div className="contentContainer">
       <div className="artContainer">
-        <div className="artSection">
-          <h1>Posters</h1>
-          <Gallery images={posters} enableImageSelection={false} />
-        </div>
-        <div className="artSection">
-          <h1>Drawings</h1>
-          <Gallery images={IMAGES} enableImageSelection={false} />
-        </div>
-        <div className="artSection">
-          <h1>Paintings</h1>
-          <Gallery images={IMAGES} enableImageSelection={false} />
-        </div>
-        <Footer />
+        {artwork.data.map(item => (
+          <div data-aos="fade-in" className="artSection">
+            <h1>{item.title}</h1>
+            <Gallery images={item.art} enableImageSelection={false} />
+          </div>
+        ))}
+        <Footer data-aos="fade-in" />
       </div>
     </div>
   );
--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../style.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init({
+  once: true, // whether animation should happen only once - while scrolling down
+});
 
 const Home = () => {
   return (
