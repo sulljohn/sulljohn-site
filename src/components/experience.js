6575e3660a3a7e170ba864529ddfca80c0c4b397
John Sullivan <sulljohn12@gmail.com>
2019-07-19 23:29:38 -0400

    Compartmentalized some of the files

--- src/components/experience.js
+++ src/components/experience.js
@@ -7,6 +7,8 @@ import SchoolIcon from '@material-ui/icons/School';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Footer from './footer';
+import experience from '../data/experience';
+import skills from '../data/skills';
 
 // TODO: modify the timeline; add the skills at the bottom; decide whether to replace the other link to LinkedIn
 
@@ -16,90 +18,23 @@ const Experience = () => {
       <div className="timeLineContainer">
         <h1>Work experience & Education</h1>
         <VerticalTimeline>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2011 - present"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<WorkIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">Creative Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-            <p>
-              Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2010 - 2011"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<WorkIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">Art Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-            <p>
-              Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2008 - 2010"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<WorkIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">Web Designer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-            <p>
-              User Experience, Visual Design
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2006 - 2008"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<WorkIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">Web Designer</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-            <p>
-              User Experience, Visual Design
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            date="April 2013"
-            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-            icon={<SchoolIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-            <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-            <p>
-              Strategy, Social Media
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            date="November 2012"
-            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-            icon={<SchoolIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-            <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-            <p>
-              Creative Direction, User Experience, Visual Design
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            date="2002 - 2006"
-            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-            icon={<SchoolIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-            <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-            <p>
-              Creative Direction, Visual Design
-            </p>
-          </VerticalTimelineElement>
+          {experience.data.map(item => (
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date={item.date}
+              iconStyle={{ background: `${item.iconBackground}`, color: `${item.iconColor}` }}
+              // eslint-disable-next-line no-nested-ternary
+              icon={item.icon === 'work' ? <WorkIcon />
+                : item.icon === 'school' ? <SchoolIcon />
+                  : <StarIcon />}
+            >
+              <h3 className="vertical-timeline-element-title">Creative Director</h3>
+              <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+              <p>
+                Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+              </p>
+            </VerticalTimelineElement>
+          ))}
           <VerticalTimelineElement
             iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
             icon={<StarIcon />}
@@ -109,18 +44,11 @@ const Experience = () => {
       <div data-aos="fade-in" className="skillContainer">
         <h1>Skills</h1>
         <div className="skillCards">
-          <Card>
-            <CardContent>test</CardContent>
-          </Card>
-          <Card>
-            <CardContent>test</CardContent>
-          </Card>
-          <Card>
-            <CardContent>test</CardContent>
-          </Card>
-          <Card>
-            <CardContent>test</CardContent>
-          </Card>
+          {skills.data.map(item => (
+            <Card>
+              <CardContent>{item.text}</CardContent>
+            </Card>
+          ))}
         </div>
       </div>
       <Footer data-aos="fade-in" />
