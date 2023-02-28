1ca410c28aa374d539bbe92a7006dcf13072dfa6
John Sullivan <sulljohn12@gmail.com>
2023-02-28 12:49:49 -0800

    Modal working

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import {
   Typography, Card, CardContent, CardHeader, Grid, CardActions, Button, CardMedia,
+  Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Slide,
 } from '@mui/material';
+import { TransitionProps } from '@mui/material/transitions';
 import projects from '../data/projects.json';
 
 const req = require.context('../res/projects/', false, /\.(jpe?g|png|gif|svg)$/);
 
+// Source: https://mui.com/material-ui/react-dialog/#transitions
+const Transition = React.forwardRef((
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+  // eslint-disable-next-line react/jsx-props-no-spreading
+) => <Slide direction="up" ref={ref} {...props} />);
+
 // Card source: https://www.youtube.com/watch?v=UNCq01LNNrg
 export default function Projects() {
+  const [open, setOpen] = React.useState(false); // Whether modal open or not
+  const [project, setProject] = React.useState(projects.data[0]); // Store project for modal
+
+  const handleClickOpen = (index: number) => {
+    setOpen(true);
+    setProject(projects.data[index]);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Card sx={{ my: 2 }}>
       <CardHeader title="Projects" sx={{ pb: 0 }} />
@@ -15,7 +38,7 @@ export default function Projects() {
         <Typography component="div" variant="body1" sx={{ my: 1 }}>
           <Grid container spacing={2}>
             {
-              projects.data.map((item) => (
+              projects.data.map((item, index) => (
                 <Grid item xs={12} sm={6} md={4}>
                   <Card sx={{ my: 2 }}>
                     <CardMedia
@@ -33,15 +56,36 @@ export default function Projects() {
                       </Typography>
                     </CardContent>
                     <CardActions>
-                      <Button size="small">Link</Button>
-                      <Button size="small">Code</Button>
+                      {item.link !== '' ? <Button size="small">Link</Button> : ''}
+                      {item.code !== '' ? <Button size="small">Code</Button> : ''}
+                      <Button size="small" onClick={() => handleClickOpen(index)}>More Info</Button>
                     </CardActions>
                   </Card>
+
                 </Grid>
               ))
             }
           </Grid>
         </Typography>
+        <Dialog
+          open={open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={handleClose}
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle>{project.title}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-slide-description">
+              Let Google help apps determine location. This means sending anonymous
+              location data to Google, even when no apps are running.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Disagree</Button>
+            <Button onClick={handleClose}>Agree</Button>
+          </DialogActions>
+        </Dialog>
       </CardContent>
     </Card>
   );
