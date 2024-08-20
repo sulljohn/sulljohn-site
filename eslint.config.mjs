4c0dce48dfd30d1508a5b8d5ea261ab2c6050d90
John Sullivan <sulljohn12@gmail.com>
2024-08-20 00:22:31 -0700

    simpler

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,8 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default [
-    {
-        files: ['src/**/*.{js,ts,tsx}'], // Target .js, .ts, and .tsx files in src/*
-        ignorePatterns: ['reportWebVitals.ts'], // Ignore the specified file
-        ...tseslint.config(
-            eslint.configs.recommended,
-            ...tseslint.configs.strict,
-            ...tseslint.configs.stylistic,
-        ),
-    },
-];
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
+);
