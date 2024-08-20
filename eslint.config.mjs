2e1db0cc7caaa1a5d4e3f12798a0376e05846af7
John Sullivan <sulljohn12@gmail.com>
2024-08-20 00:39:46 -0700

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,21 @@ export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.strict,
     ...tseslint.configs.stylistic,
+    {
+        ignorePatterns: ['reportWebVitals.ts'],
+        rules: {
+            'indent': ['error', 2, { SwitchCase: 1 }],
+            'linebreak-style': ['error', 'unix'],
+            'object-curly-spacing': ['error', 'always'],
+            'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0, maxBOF: 0 }],
+            'keyword-spacing': ['error', { before: true, after: true }],
+            'array-bracket-spacing': ['error', 'never'],
+            'no-trailing-spaces': 'error',
+            'space-before-function-paren': ['error', 'never'],
+            'eol-last': ['error', 'always'],
+            'quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+            'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+            'no-multi-spaces': 'error',
+        }
+    }
 );
